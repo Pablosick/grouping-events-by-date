d725e4530d6801762ad5cb7d2a28d890baffcc3c
Pavel Bochkarev <bochkarevpasha18@gmail.com>
2023-04-12 02:28:30 +0400

    Adding level private acess

--- baseclass_events.py
+++ baseclass_events.py
@@ -7,7 +7,7 @@ from src.enums.type_events_enum import EventType
 
 class EventsHandler:
 
-    result_json = {}
+    __result_json = {}
 
     def __init__(self, input_json, o_json):
         self.input_json = input_json
@@ -15,21 +15,25 @@ class EventsHandler:
 
 
     def get_result(self):
-        raw_list_events = self.running_raw_json()
-        return self.working_with_events(raw_list_events)
+        """
+        The function returns a grouped list of events by date
+        :return: JSON
+        """
+        raw_list_events = self.__running_raw_json()
+        return self.__working_with_events(raw_list_events)
 
 
-    def working_with_events(self, raw_list):
+    def __working_with_events(self, raw_list):
         for item in range(len(raw_list)):
-            if self.validate_keys(raw_list[item]) == raw_list[item]:
-                self.result_json.setdefault(self.date_keys(raw_list[item]), [])
-                self.result_json[self.date_keys(raw_list[item])].append(raw_list[item])
-                self.result_json[self.date_keys(raw_list[item])] = sorted(self.result_json[self.date_keys(raw_list[item])],
-                                                                          key=lambda x: datetime.datetime.strptime(x['date'], '%Y-%m-%d %H:%M:%S.%f'), reverse=False)
-        return self.output_json(self.result_json)
+            if self.__validate_keys(raw_list[item]) == raw_list[item]:
+                self.__result_json.setdefault(self.__date_keys(raw_list[item]), [])
+                self.__result_json[self.__date_keys(raw_list[item])].append(raw_list[item])
+                self.__result_json[self.__date_keys(raw_list[item])] = sorted(self.__result_json[self.__date_keys(raw_list[
+                                                                                                                   item])], key=lambda x: datetime.datetime.strptime(x['date'], '%Y-%m-%d %H:%M:%S.%f'), reverse=False)
+        return self.__output_json(self.__result_json)
 
 
-    def validate_keys(self, event):
+    def __validate_keys(self, event):
         validate_type = event['type'] in EventType[event['type']].value and event['type'] not in EventType.other.value
 
         check_str_name = ' '.join([i for i in event['name'].split() if len(i) < 20 and i.isalnum()])
@@ -41,18 +45,18 @@ class EventsHandler:
             return event
 
 
-    def date_keys(self, event):
+    def __date_keys(self, event):
         new_keys_json = datetime.datetime.strptime(event['date'], '%Y-%m-%d %H:%M:%S.%f')
         return str(new_keys_json.date())
 
 
-    def running_raw_json(self):
+    def __running_raw_json(self):
         with open(self.input_json, 'r', encoding='utf8') as raw_json:
             json_in_dict = json.load(raw_json)['events']
         return json_in_dict
 
 
-    def output_json(self, json_structure):
+    def __output_json(self, json_structure):
         with open(self.o_json, 'w',  encoding='utf8') as output_json:
             result_dict_in_json = json.dump(json_structure, output_json)
         return result_dict_in_json
