473dee3f8c46038df5e5414e4faf5d4728699887
Pavel Bochkarev <bochkarevpasha18@gmail.com>
2023-04-13 21:26:23 +0400

    Events generation class

---
+++ gen_events.py
@@ -0,0 +1,75 @@
+import datetime
+import json
+import random
+import string
+
+from faker import Faker
+
+from src.enums.type_events_enum import EventType
+
+
+class EventsGeneration:
+
+    __fake = Faker()  # Экземпляр класса Faker
+    __listEvents = []  # Список событий
+    __oneEvent = dict.fromkeys(["date", "type", "name", "members", "place"])  # Генерация ключей одного события
+
+
+    def __init__(self, start_date, final_date):
+        self.start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+        self.final_date = datetime.datetime.strptime(final_date, "%Y-%m-%d")
+
+
+    def generation_json(self):
+        result_dictionary = self.main_dictionary_generation()
+        return self.__convert_to_json_file(result_dictionary)
+
+
+    def main_dictionary_generation(self):
+        for rand_events in range(random.randint(1,  50)):
+            self.__listEvents.append(self.__adding_a_single_event(self.__oneEvent.copy()))
+        output_dictionary = {"events": self.__listEvents}
+        return output_dictionary
+
+
+    def __adding_a_single_event(self, input_dict):
+        for keys in input_dict:
+            if keys == "date":
+                self.__date_generation(input_dict)
+            elif keys == "type":
+                self.__type_generation(input_dict)
+            elif keys == "name":
+                self.__name_generation(input_dict)
+            elif keys == "place":
+                self.__place_generation(input_dict)
+        return input_dict
+
+
+    def __date_generation(self, date_key):
+        date_range = self.final_date - self.start_date
+        seconds_range = (date_range.days * 24 * 60 * 60) + date_range.seconds
+        date_key["date"] = str(self.start_date+datetime.timedelta(seconds=random.randrange(seconds_range)))
+        return date_key
+
+
+    def __type_generation(self, type_key):
+        type_key["type"] = random.choice([i.value for i in EventType])
+        return type_key
+
+
+    def __name_generation(self, name_key):
+        rand_name = ''.join(random.choice(string.ascii_lowercase + string.digits + ' ') for j in range(random.randint(0, 150)))
+        name_key["name"] = rand_name
+        return name_key
+
+
+    def __place_generation(self, place_key):
+        rand_place = ["telegram", "zoom", ''.join(random.choice(string.ascii_lowercase) for j in range(random.randint(0, 15)))]
+        place_key["place"] = rand_place
+        return place_key
+
+
+    def __convert_to_json_file(self, final_dictionary):
+        with open('./mok_json/generator.json', 'w') as g_json:
+            json.dump(final_dictionary, g_json, indent=4)
+
